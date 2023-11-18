494af2c4f83a7bbab20ebfd8036f197b0d575907
shivank0311 <shivank0311@gmail.com>
2023-11-18 16:01:40 +0530

    Branch2

---
+++ SimpleObject.java
@@ -0,0 +1,19 @@
+
+
+public class SimpleObject {
+	SimpleObject()
+	{
+		System.out.println("No Argument :");
+	}
+	SimpleObject(int obj1)
+	{
+		System.out.println("Enter object :" );
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleObject obj = new SimpleObject();
+
+	}
+
+}
